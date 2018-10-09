f195afd01ec3ba634a9920ab093a72f38bd3a800
M1ndBlast <gustavopdzz0@gmail.com>
2018-10-09 13:12:51 -0500

    Pruebas de encriptacion conv vector

--- app.js
+++ app.js
@@ -1,152 +1,41 @@
-//                              Importacion de modulos de node a ocupar en este archivo
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// Importacion e inicializacion del servidor
-const express = require('express')
-//Utilizacion de bodyparser para pasar informacion en formularios y archivos json que vengan desde la url
-const bodyParser = require('body-parser')
-// Uso de fileupload para subir archivos al Servidor
-const fileUpload = require('express-fileupload')
-// Uso de crypto para desencriptar mamadas
-const crypto = require('crypto')
-// Uso para re convertir archivos
-const fs = require('fs')
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-var mysql = require('mysql');
+var app = express();
 
-var MyConnection = mysql.createConnection({
-   host: 'localhost',
-   user: 'root',
-   password: 'n0m3l0',
-  database: 'AESDB'
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-const port = 8080
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//Declaracion de variables y funciones que ocupara el servidor que nos povee Express
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-// Declaracion e inicializacion del servidor Express
-const app = express()
-// Asignacion de carpeta publica a la cual los usuarios del servidor pueden acceder
-app.use(express.static('public'))
-// Asignacion de body-parser en el servidor para que se puedan "Atrapar" los valores mandados atraves de la url y de los archivos .json
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-// Asignacion de fileupload para poder subir archivos al servidor por parte del cliente
-app.use(fileUpload())
-// Declaracion y asignacion de la clase de vistas dinamicas que se va ocupar
-app.set('view engine', 'pug')
-// Asignacion del puerto por el que el servidor escuchara las solicitudes del cliente (Es forsozo elegir uno, no puedeescuchar por cualquier puerto)
-app.listen(port, () =>
-{
-    console.log('Servidor corriendo en: \n "http://localhost:'+port+'"')
-})
-
-// Asignacion del archivo index, que utiliza como "plantilla"/"archivo padre" el archivo layout en la url: NAMEHOST+/
-app.get('/', (req, res) =>
-{
-    res.render('index')
-})
-
-// Asignacion del proceso para cifrar un archivo en la url: NAMEHOST+/crypt
-app.get('/crypt',(req,res) =>
-{
-    var text = req.body.text,
-    pass = req.body.pass
-
-    var algorithm = 'aes-256-gcm',
-    password = '3zTvzr3p67VC61jmV54rIYu1545x4TlY',
-  // do not use a global iv for production,
-  // generate a new one for each encryption
-  iv = '60iP0h6vJoEa'
-
-  var text = 'Este si es un gatito kawai :C'
-  console.log('EL texto original es:')
-  console.log(text);
-
-  console.log('Empezar con encriptacion')
-
-  var cipher = crypto.createCipheriv(algorithm, password, iv)
-  var encrypted = cipher.update(text, 'utf8', 'hex')
-  encrypted += cipher.final('hex');
-  var tag = cipher.getAuthTag();
-  console.log('EL texto encriptado es');
-  console.log(encrypted);
-
-
-  console.log('Empezar con desencriptacion')
-
-  var decipher = crypto.createDecipheriv(algorithm, password, iv)
-  decipher.setAuthTag(tag);
-  var dec = decipher.update(encrypted, 'hex', 'utf8')
-  dec += decipher.final('utf8');
-  console.log('EL texto desencriptado es');
-  console.log(dec);
-
-})
-
-// Asignacion del proceso para descifrar un archivo en la url: NAMEHOST+/decrypt
-// repoyo del /crypt pero sustituyendo  cifrado por descifrado y partiendo de un hex a un utf-8
-app.post('/decrypt',(req,res) =>
-{
-    let file = req.files.file
-    let key = req.body.key
-
-    let ok = valFile(file)
-    if (!ok)
-        res.render('error')
-    else
-    {
-        ok = valKey(key)
-        if (!ok) res.render('error')
-        else
-        {
-            console.log("El original en Buffer es: "+ file.data)
-
-            var decipher = crypto.createDecipher('DES-ECB', key)
-            var dec = decipher.update(file.data, 'hex', 'utf8')
-            // Uso de  try-catch en para atrapar error en caso de que ingrese una clave erronea (Que no sea la misma llave con la que se encipto)
-            try
-            {
-              dec += decipher.final('utf8')
-            } catch (e) {
-              console.log(e);
-              console.log('Algo salio mal');
-              res.render('error')
-            }
-            console.log("El desencriptado en Buffer es: "+ dec)
-
-            fs.writeFile(`${file.name}`, dec, 'utf8', (err) =>
-            {
-                if (err) throw err
-                res.download(`${file.name}`, file.name, () =>
-                {
-                    fs.unlink(`${file.name}`, (err) =>
-                    {
-                        if (err) throw err
-
-                        console.log('Archivo eliminado del servicio')
-                    })
-                })
-            })
-        }
-    }
-
-})
-
-// Funcion para validar que la terminacion del archivo es unicamente txt
-function valFile(file)
-{
-    let extension = file.name.substring(file.name.lastIndexOf('.')+1, file.name.length)
-    console.log(`La extension es: ${extension}`)
-    return (extension == 'txt')? true : false
-}
-// Funcion para validar que la llave de encriptacion cumple con un formato estandar y no tenga caracteres desconocidos
-function valKey(key)
-{
-    let patron = /[\w .]{8}/
-    console.log('La key cumple ?:' + patron.test(key))
-    return patron.test(key)
-}
-
-// more info en:
-//https://nodejs.org/api/crypto.html#crypto_class_cipher
+module.exports = app;
